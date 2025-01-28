048e94ef557fbd897c2413d0e94978c73f20b0ef
ahmedbellaaj10 <ahmed-bellaaj@outlook.com>
2025-01-28 10:04:38 +0100

    update resume

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -19,7 +19,7 @@ export default function Testimonials() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 stagger">
+        <div className="flex flex-wrap justify-center gap-8">
           {testimonials.map((testimonial, index) => (
             <TestimonialCard
               key={index}
